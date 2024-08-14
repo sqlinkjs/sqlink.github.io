178ff999e8f2201cee052b6462a394c6bae46cd5
santhosh <santhosh4u1997>
2024-08-14 18:49:52 +0530

    update on copy clipboard

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ document.getElementById("success-message").style.display = "none";
 
 
 function onCopyClick() {
+    let cp_text = navigator.platform.toLowerCase().startsWith("win") ? "npm install -g sqlink" : "sudo npm install -g sqlink"
+    navigator.clipboard.writeText(cp_text)
     document.getElementById("default-message").style.display = "none";
     document.getElementById("success-message").style.display = "";
     setTimeout(() => {
